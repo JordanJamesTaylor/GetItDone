1509230ebf023496fe0c2bd43ac9a897003d80f0
JordanJamesTaylor <jordjamestaylor@gmail.com>
2023-06-23 16:09:24 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <main className='main'>
+      <h1 style={{ color: 'green' }}>Hello World!</h1>
+    </main>
+  );
+};
