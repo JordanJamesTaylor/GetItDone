2229eb0c5567d39f4539034b98994b0d22f0cc9e
JordanJamesTaylor <jordjamestaylor@gmail.com>
2023-06-23 19:24:44 -0400

    Navbar styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,29 @@ import './navbar.css';
 export default function Havbar() {
     return (
         <nav className='navbar'>
-            Navbar
+            <div className='nav-left'>
+                <span class="material-symbols-outlined">
+                    home
+                </span>
+                <input className='nav-search-bar' type="text" placeholder="Search.." />
+            </div>
+
+            <span className='nav-bar-span' />
+
+            <div className='nav-right'>
+                <div class="material-symbols-outlined">
+                    add
+                </div>
+                <div class="material-symbols-outlined">
+                    help
+                </div>
+                <div class="material-symbols-outlined">
+                    notifications
+                </div>
+                <div className='profile-nav-icon'>
+                    JT
+                </div>
+            </div>
         </nav>
     );
 };
